e53bd4ae745a980f9108ff30917120de159fb389
yathiDev <158311781+yathiDev@users.noreply.github.com>
2024-02-07 20:19:07 +0530

    Update first_script.sh

--- first_script.sh
+++ first_script.sh
@@ -1,7 +1,7 @@
 if [ "$BUILD_STATUS" = "SUCCESS" ]; then
   echo "Build was successful, running additional steps..."
   # Add additional commands here
-  touch file_yathi
+  touch cron1
 else
   echo "Build failed, skipping additional steps."
 fi
